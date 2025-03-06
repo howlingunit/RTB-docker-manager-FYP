2a706bcadba8dbc9c41bea1d073e741e2b3c1c1c
howlingunit <howlingunit3456@gmail.com>
2025-03-06 17:41:31 +0000

    feat: better tests, docker template

--- tests/endpoints_test.go
+++ tests/endpoints_test.go
@@ -1,35 +1,54 @@
 package main_test
 
 import (
+	"encoding/json"
+	"io"
 	"net/http"
-	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestTest(t *testing.T) {
 	// Create a request to the endpoint
-	req, err := http.NewRequest("GET", "/test", nil)
+	res, err := http.Get("http://localhost:8080/test") // Real API call
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	// Check the status code
+	assert.Equal(t, http.StatusOK, res.StatusCode)
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Check the response body
+	expected := "hi"
+	assert.Equal(t, expected, string(body))
+}
+
+type Challenge struct {
+	Name       string `json:"name"`
+	Difficulty string `json:"difficulty"`
+}
+
+func TestGetChallenges(t *testing.T) {
+	res, err := http.Get("http://localhost:8080/get-challenges") // Real API call
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	assert.Equal(t, http.StatusOK, res.StatusCode)
+
+	var challenges []Challenge
+	err = json.NewDecoder(res.Body).Decode(&challenges)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	// Create a ResponseRecorder to record the response
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hi"))
-	})
-
-	// Serve the HTTP request
-	handler.ServeHTTP(rr, req)
-
-	// Check the status code
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("expected status code %d, got %d", http.StatusOK, status)
-	}
-
-	// Check the response body
-	expected := "hi"
-	if rr.Body.String() != expected {
-		t.Errorf("expected response body %q, got %q", expected, rr.Body.String())
-	}
+	expected := Challenge{Name: "samba", Difficulty: "easy"}
+	assert.Contains(t, challenges, expected)
 }
